c8d7de5c8d3bec761b9a585db920433f93e89722
teivah <t.harsanyi@thebeat.co>
2020-06-30 22:16:00 +0200

    Project init

--- cmd/traverse-backwards/main.go
+++ cmd/traverse-backwards/main.go
@@ -11,14 +11,14 @@ import (
 
 func main() {
 	in := os.Args[1]
-	i, err := strconv.Atoi(in)
+	iterations, err := strconv.Atoi(in)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	s := utils.CreateSlice(i)
+	s := utils.CreateSlice(iterations)
 	sum := 0
-	for i := i - 1; i >= 0; i-- {
+	for i := iterations - 1; i >= 0; i-- {
 		sum += s[i]
 	}
 	fmt.Println(sum)
--- cmd/traverse-forwards/main.go
+++ cmd/traverse-forwards/main.go
@@ -11,14 +11,14 @@ import (
 
 func main() {
 	in := os.Args[1]
-	i, err := strconv.Atoi(in)
+	iterations, err := strconv.Atoi(in)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	s := utils.CreateSlice(i)
+	s := utils.CreateSlice(iterations)
 	sum := 0
-	for i := 0; i < i; i++ {
+	for i := 0; i < iterations; i++ {
 		sum += s[i]
 	}
 	fmt.Println(sum)
